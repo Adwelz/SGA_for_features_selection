c6c615e877612ff8b94158e9c74b575361215776
Adwelz <43895468+Adwelz@users.noreply.github.com>
2023-02-12 20:13:40 +0100

    ;p

--- Ga.py
+++ Ga.py
@@ -1,31 +1,72 @@
+# Import
+
 import numpy as np
 from LinReg import LinReg
 import random
-import configparser
 import matplotlib.pyplot as plt
+
+# Load the dataset
 
 dataset=np.loadtxt(open("dataset.txt", "rb"), delimiter=",")
 X=dataset[:,:-1]
 Y=dataset[:,-1]
 
+# LinReg
+
 lR=LinReg()
 
+# Useful functions
+
 def bitstring2number(bitstring):
+    """ Transform bitstring to number
+
+    Args:
+        - bitstring: A bitstring
+
+    Returns:
+        - The number given from his bits representation
+    """
     l = bitstring.dot(2**np.arange(bitstring.size)[::-1])
     return l*2**(7-len(bitstring))
 
 def unit_test_bitstring2number():
+    """ Unit test for bitstring2number
+
+    Returns:
+        A boolean (True if the test is pass)
+    """
     print(bitstring2number(np.array([1,0,1])) == 5*2**(7-3))
 
 def fitness_for_sinus(bitstring):
+    """ Give the fitness value of a bitstring for the sinus case
+
+    Args:
+        - bitstring: A bitstring
+
+    Returns:
+        The fitness value of the bitstring
+    """
     x = bitstring2number(bitstring)
     return np.sin(x)
 
 def unit_test_fitness_for_sinus():
+    """ Unit test for fitness_for_sinus
+
+    Returns:
+        A boolean (True if the test is pass)
+    """
     x = bitstring2number(np.array([1,0,1]))
     print(np.sin(x) == np.sin(5*2**(7-3)))
 
 def fitness_for_sinus_with_penalty(bitstring,interval,penalty_rate):
+    """ Give the fitness value with the penalty of a bitstring for the sinus case
+
+    Args:
+        - bitstring: A bitstring
+
+    Returns:
+        The fitness value with the penalty of the bitstring
+    """
     x = bitstring2number(bitstring)
     if interval[0]<=x<=interval[1]:
         return np.sin(x)
@@ -35,20 +76,39 @@ def fitness_for_sinus_with_penalty(bitstring,interval,penalty_rate):
         return np.sin(x)-penalty_rate*(x-interval[1])
 
 def population_entropy(population):
+    """ Give the entropy of a given population
+
+    Args:
+        - population: A population
+
+    Returns:
+        The entropy of the population
+    """
     bit_proba = np.mean(population,axis=1)
     return - np.sum(bit_proba.dot(np.log(bit_proba)))
 
 def fitness_for_data_case(bitstring):
+    """ Give the fitness value of a bitstring for the data case
+
+    Args:
+        - bitstring: A bitstring
+
+    Returns:
+        The fitness value of the bitstring
+    """
     bitstring = np.array(bitstring)
 
     x_filtered=lR.get_columns(X,bitstring)
     return 1/lR.get_fitness(x_filtered, Y,4)
+
+# Initiate the population
 
 def initiate_population(n,bitstring_lentgh):
     """Initiate the population
     
     Args:
         - n: The number of individual
+        - bitstring_lentgh: The lentgth of a bitstring
 
     Returns:
         - A random population of n individual
@@ -58,15 +118,18 @@ def initiate_population(n,bitstring_lentgh):
         p=np.random.randint(2, size=(n,bitstring_lentgh))
     return p
 
+# Selection
+
 def trivial_selection(population,n,fitness_fonction):
     """Trivial selection: takes the best individual
     
     Args:
         - population: The population
         - n: The number of individual we keep
+        - fitness_fonction: fitness_fonction
 
     Returns:
-        - A population of the n best individual of the previous population
+        - A population of the n best individual of the previous population and the previous population without these individual
     """
     
     F=[]
@@ -82,12 +145,15 @@ def trivial_selection(population,n,fitness_fonction):
     R=R.astype(int)
     return population[F],population[R]
 
+# Compete
+
 def trivial_compete(i1,i2,fitness_fonction):
     """Compete between two individual
 
     Args:
         - i1: An individual
         - i2: An individual
+        - fitness_fonction: the fitness fonction
 
     Returns:
         - The best individual
@@ -102,6 +168,8 @@ def trivial_compete(i1,i2,fitness_fonction):
         return i1
     else :
         return i2
+
+# Crossover
 
 def random_crossover(p1,p2):
     """Crossover between two parent
@@ -124,14 +192,17 @@ def random_crossover(p1,p2):
             c2.append(p1[i])
     return c1,c2
 
+# Mutation
+
 def normal_mutation(c,probability):
     """Mutation of an individual
 
     Args:
-        -c: An individual
+        - c: An individual
+        - probability: A probability
 
     Returns:
-        An individual with a probability of 1/2 to be a mutation of c else it return c
+        An individual with the given probability to be a mutation of c else it return c
         (only one elements of c is mutated in case of mutation)
     """
     mutate_c=c
@@ -145,10 +216,31 @@ def normal_mutation(c,probability):
     
     return mutate_c
 
+# Genetic algorithm class
 class Ga:
+    """ Genetic algorithm class 
+    """
     def __init__(self,crossover_type,crossover_rate,mutation_type,mutate_rate,selection_type,fitness_fonction,crowding_option=False, 
                 compete_type='none',interval='none',penalty_rate='0') -> None:
-        
+        """ Constructor of the genetic algorithm class
+
+        Args:
+            - self: The genetic algorithm
+            - crossover_type: The type of the crossover
+            - crossover_rate: The rate of the crossover
+            - mutation_type: The type of the mutation
+            - mutate_rate: The rate of the mutation
+            - selection_type: The type of the selection
+            - fitness_fonction: The fitness function
+            - crowding_option: Option whether using crowding or not
+            - compete_type: The type of competion between parent and child
+            - interval: The interval of the solution space (for the sinus case)
+            - penalty_rate: The penalty rate for restricting the solution to the interval (for the sinus case)
+
+        Returns:
+            The genetic algorithm with the given parameters
+        """
+
         self.crossover_rate = float(crossover_rate)
         self.mutate_rate = float(mutate_rate)
         self.crowding_option = crowding_option
@@ -185,11 +277,11 @@ class Ga:
             self.compete = trivial_compete
 
     def one_cycle_function(self,p1,p2):
-        """Crossover of parent, mutation of their child and compete between parents and childs
+        """Crossover of parent, mutation of their child and compete between parents and childs if the crowding option is on
 
         Args:
-            -p1: A parent
-            -p2: A parent
+            - p1: A parent
+            - p2: A parent
         
         Returns:
             Individuals that result after crossover, mutation and compete
@@ -219,6 +311,16 @@ class Ga:
             return c1,c2
 
     def one_cycle(self, population, number_of_individuals, number_of_parents):
+        """ What happen in one cycle of the genetic algorithm
+
+        Args:
+            - population: A population
+            - number_of_individuals: The number of individuals of the current population
+            - number_of_parents: The number of parents selected
+        
+        Returns:
+            The next population
+        """
         childs=[]
         parents,pop_without_parent=self.selection(population,number_of_parents,self.fitness_function)
         if self.crowding_option == True:
@@ -234,6 +336,16 @@ class Ga:
         return population
 
     def run(self, number_of_individuals, number_of_cycle,bitstring_length):
+        """ Run the genetic algorithm
+
+        Args:
+            - population: A population
+            - number_of_individuals: The number of individuals of the current population
+            - number_of_parents: The number of parents selected
+        
+        Returns:
+            The next population
+        """
         number_of_parents = int(number_of_individuals*self.crossover_rate)
         entropy = []
         population=initiate_population(number_of_individuals,bitstring_length)
